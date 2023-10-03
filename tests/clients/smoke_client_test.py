5d517b54b6c9d0f7a53ba0d515036c7df07fccce
ReGuardian <danilasuhoparov@yandex.ru>
2023-10-03 19:07:12 +0300

    добавлена негативная проверка метода получения информации о клиенте

--- tests/clients/smoke_client_test.py
+++ tests/clients/smoke_client_test.py
@@ -3,15 +3,16 @@ import pytest
 import requests
 import dotenv
 from tests.clients.requests_test_methods import ClientInfoMethods
+from tests.conftest import jwt_gen
 
 dotenv.load_dotenv()
 
 
 @pytest.mark.usefixtures('temp_db')
 class TestMain:
-    # @pytest.mark.debug
+    @pytest.mark.debug
     def test_starter_eligible_true(self, temp_db):
-        full_url = os.getenv("CLIENT_SERVICE_TEST_URL") + os.getenv("STARTER_ELIGIBLE_PATH")
+        full_url = os.getenv("CLIENT_API_ORDERS_URL") + os.getenv("STARTER_ELIGIBLE_PATH")
         full_auth = "Bearer " + temp_db.get("new_client_token")
         headers = {"Authorization": full_auth}
         response = requests.get(url=full_url, headers=headers)
@@ -23,16 +24,21 @@ class TestMain:
 
     @pytest.mark.debug
     def test_client_info_positive(self, temp_db):
-        full_url = os.getenv("CLIENT_SERVICE_TEST_URL") + os.getenv("CLIENT_INFO_PATH")
+        full_url = os.getenv("CLIENT_API_CLIENTS_URL") + os.getenv("CLIENT_INFO_PATH")
         full_auth = "Bearer " + temp_db.get("new_client_token")
-        print(full_auth)
+        # print(full_auth)
         headers = {"Authorization": full_auth}
         response = requests.get(url=full_url, headers=headers)
         response_body = response.json()
         ClientInfoMethods.client_info_asserting(response=response_body, db_values=temp_db.get("db_client_data"))
 
+    # @pytest.mark.debug
+    def test_client_info_negative(self, temp_db):
+        full_url = os.getenv("CLIENT_API_CLIENTS_URL") + os.getenv("CLIENT_INFO_PATH")
+        full_auth = "Bearer " + jwt_gen("definitely_wrong_client_id")
+        # print(full_auth)
+        headers = {"Authorization": full_auth}
+        response = requests.get(url=full_url, headers=headers)
 
-        # response_body = response.json()
-        # print("\n---------------------------------------------------------")
-        # print(response_body)
-        # assert response.status_code == 200
+        assert response.status_code == 401, "ERROR | Wrong status code"
+        assert response.json() == {'detail': 'Unauthorized'}
